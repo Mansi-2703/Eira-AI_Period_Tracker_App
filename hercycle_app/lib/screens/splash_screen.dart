7d49461d11044283e8d45222bb37274e9ccc4441
Mansi More <moremanasi249@gmail.com>
2026-03-18 01:29:25 +0530

    made changes in launch screens

--- hercycle_app/lib/screens/splash_screen.dart
+++ hercycle_app/lib/screens/splash_screen.dart
@@ -10,11 +10,27 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _breathingController;
+  late Animation<double> _breathingAnimation;
+
   @override
   void initState() {
     super.initState();
+    _setupBreathingAnimation();
     _scheduleNextRoute();
+  }
+
+  void _setupBreathingAnimation() {
+    _breathingController = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    _breathingAnimation = Tween<double>(begin: 1.0, end: 1.15).animate(
+      CurvedAnimation(parent: _breathingController, curve: Curves.easeInOut),
+    );
   }
 
   Future<void> _scheduleNextRoute() async {
@@ -27,6 +43,12 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   @override
+  void dispose() {
+    _breathingController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: HerCyclePalette.light,
@@ -34,12 +56,16 @@ class _SplashScreenState extends State<SplashScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-                    Image.asset(
-                    'assets/images/logo.png',
-                    width: 120,
-                    height: 120,
-                    fit: BoxFit.contain,
-                  ),
+            // Animated breathing logo
+            ScaleTransition(
+              scale: _breathingAnimation,
+              child: Image.asset(
+                'assets/images/logo.png',
+                width: 120,
+                height: 120,
+                fit: BoxFit.contain,
+              ),
+            ),
             const SizedBox(height: 16),
             Text(
               'Eira',
